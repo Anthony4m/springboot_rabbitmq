df0e3f73ba1ef7129ba299ba1c6550ca7a21bfae
Anthony4m <amoahanthony41@gmail.com>
2022-07-13 19:30:38 +0000

    first commit

---
+++ spring-rabit/src/main/java/com/example/springrabit/SpringRabitApplication.java
@@ -0,0 +1,13 @@
+package com.example.springrabit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRabitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringRabitApplication.class, args);
+	}
+
+}
---
+++ spring-rabit/src/test/java/com/example/springrabit/SpringRabitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springrabit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRabitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
